94f6f751d73219d9046c9bf98646b687c9911e88
irwanphan <irwanphan@gmail.com>
2024-07-24 08:48:49 +0700

    update heroes

---
+++ _next/static/chunks/pages/story/sahabatgemarikan-6e81f4cbe68b55c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[858],{3350:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/story/sahabatgemarikan",function(){return t(6625)}])},6625:function(n,u,t){"use strict";t.r(u);var _=t(5893),e=t(8527);u.default=()=>(0,_.jsx)(e.xu,{children:"SGI"})}},function(n){n.O(0,[527,888,774,179],function(){return n(n.s=3350)}),_N_E=n.O()}]);
